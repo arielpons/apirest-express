7a7051f3a8ab009dc4e8282313825cd3b692669d
Ariel Pons <ariel_pons@hotmail.com>
2021-07-19 20:12:01 -0300

    Routes renamed

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router();
 
-const { 
+const {
+    getAllUsuarios,
     getUsuario,
     postUsuario,
     putUsuario,
@@ -14,6 +15,7 @@ const {
     validarPutUsuario,
 } = require('../middlewares/validarUsuario')
 
+router.get("/", getAllUsuarios);
 router.get('/:_id', validarIdParamUsuario, getUsuario);
 router.post('/', validarPostUsuario, postUsuario);
 router.put('/:_id',validarIdParamUsuario, validarPutUsuario, putUsuario);
